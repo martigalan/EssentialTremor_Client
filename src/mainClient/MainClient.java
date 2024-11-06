b3df78165b83ed3982874fbe5397acb9e6c577ba
martigalan <martina.galaningelmo@usp.ceu.es>
2024-11-06 23:12:17 +0100

    add register and login obtaining info of server

--- src/mainClient/MainClient.java
+++ src/mainClient/MainClient.java
@@ -6,6 +6,8 @@ import pojos.User;
 
 import java.io.*;
 import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -30,8 +32,6 @@ public class MainClient {
             inputStream = socket.getInputStream();
             boolean connection = true;
 
-            //1º menú register/login
-            //2º si quiere register, le manda al server que es patient y sus datos
             //3º si quiere login, coger de la bbdd del server el username y password y devuelvo TRUE o FALSE
 
             int option;
@@ -63,53 +63,111 @@ public class MainClient {
                             break;
                     }
                 }
-
-
-            } catch (NumberFormatException e) {
+            } catch (NumberFormatException | NoSuchAlgorithmException e) {
                 System.out.println("  NOT A NUMBER. Closing application... \n");
                 sc.close();
             }
-
         } catch (IOException e) {
             System.out.println("Error connecting to the server.");
             e.printStackTrace();
         } finally {
             releaseResourcesClient(inputStream, socket);
         }
-
     }
 
-    public static void login() throws IOException {
+    public static void registerPatient() throws IOException, NoSuchAlgorithmException {
+        System.out.println("Please enter patient details:");
+        String name = "", surname = "", username = "", password = "";
+        boolean geneticBackground = false;
+
+        while (true) {
+            System.out.print("Name: ");
+            name = sc.nextLine().trim();
+            if (!name.isEmpty()) break;
+            System.out.println("Invalid name. Please enter a valid name.");
+        }
+
+        while (true) {
+            System.out.print("Surname: ");
+            surname = sc.nextLine().trim();
+            if (!surname.isEmpty()) break;
+            System.out.println("Invalid surname. Please enter a valid surname.");
+        }
+
+        while (true) {
+            System.out.print("Genetic Background (true/false): ");
+            String input = sc.nextLine().trim().toLowerCase();
+            if (input.equals("true") || input.equals("false")) {
+                geneticBackground = Boolean.parseBoolean(input);
+                break;
+            }
+            System.out.println("Invalid input. Please enter 'true' or 'false'.");
+        }
+
+        while (true) {
+            System.out.print("Username: ");
+            username = sc.nextLine().trim();
+            if (!username.isEmpty()) break;
+            System.out.println("Invalid username. Please enter a valid username.");
+        }
+
+        System.out.print("Password: ");
+        password = sc.nextLine();
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(password.getBytes());
+        byte[] hash = md.digest();
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hash) {
+            sb.append(String.format("%02x", b));
+        }
+        String encryptedPassword = sb.toString();
+
+        String patientData = name + "|" + surname + "|" + geneticBackground + "|" + username + "|" + encryptedPassword + "|patient";
+        printWriter.println(patientData);  //Send to server
+        System.out.println("Patient and user data sent to the server for registration.");
+    }
+
+    public static void login() throws IOException, NoSuchAlgorithmException {
         Scanner sc = new Scanner(System.in);
         System.out.print("Username: ");
         String username = sc.nextLine();
         System.out.print("Password: ");
         String password = sc.nextLine();
-        String patientData = username + "|" + password;
-        printWriter.println(patientData);
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(password.getBytes());
+        byte[] hashedPassword = md.digest();
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hashedPassword) {
+            sb.append(String.format("%02x", b));
+        }
+        String encryptedPassword = sb.toString();
+
+        String loginData = username + "|" + encryptedPassword;
+        printWriter.println(loginData);
         System.out.println("User data sent to the server for login.");
 
-        //TODO comprobar q existe
-        //TODO cuando existe
-        //devolver datos paciente en la bbdd y inicializar
-        //patient = new Patient(...);
-        menuUser();
+        String response = bufferedReader.readLine(); //receive response from server
+        if (response.equals("LOGIN_SUCCESS")) {
+            String roleResponse = bufferedReader.readLine();
+            if (roleResponse.equals("WELCOME_PATIENT")) {
+                String patientData = bufferedReader.readLine();
+                String[] patientInfo = patientData.split("\\|");
+                Patient patient = new Patient(patientInfo[0], patientInfo[1], Boolean.parseBoolean(patientInfo[2]));
+                System.out.println("Welcome, " + patient.getName() + " " + patient.getSurname());
+                menuUser();
+            }
+        } else {
+            System.out.println("Login failed. Please try again.");
+        }
     }
+
 
     public static void menuUser(){
         try {
             control = true;
             while (control) {
-                System.out.println("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-                System.out.println("@@                                                                  @@");
-                System.out.println("@@                 Welcome.                                         @@");
-                System.out.println("@@                 1. Open Medical Record                           @@");
-                System.out.println("@@                 2. Send Medical Record                           @@");
-                System.out.println("@@                 0. Exit                                          @@");
-                System.out.println("@@                                                                  @@");
-                System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-                System.out.print("\nSelect an option: ");
-
+                printPatientMenu();
                 try {
                     option = sc.nextInt();
                 } catch (InputMismatchException e) {
@@ -132,7 +190,6 @@ public class MainClient {
                         break;
                 }
             }
-
         } catch (NumberFormatException e) {
             System.out.println("  NOT A NUMBER. Closing application... \n");
             sc.close();
@@ -140,6 +197,7 @@ public class MainClient {
             throw new RuntimeException(e);
         }
     }
+
     public static void printLoginMenu(){
         System.out.println("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
         System.out.println("@@                                                                  @@");
@@ -151,6 +209,19 @@ public class MainClient {
         System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
         System.out.print("\nSelect an option: ");
     }
+
+    public static void printPatientMenu() {
+        System.out.println("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println("@@                                                                  @@");
+        System.out.println("@@                 Welcome.                                         @@");
+        System.out.println("@@                 1. Open Medical Record                           @@");
+        System.out.println("@@                 2. Send Medical Record                           @@");
+        System.out.println("@@                 0. Exit                                          @@");
+        System.out.println("@@                                                                  @@");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.print("\nSelect an option: ");
+    }
+
     private static void releaseResourcesClient(InputStream inputStream, Socket socket) {
         try {
             inputStream.close();
@@ -164,51 +235,6 @@ public class MainClient {
             Logger.getLogger(MainClient.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-    public static void registerPatient() throws IOException {
-        System.out.println("Please enter patient details:");
-        String name = "";
-        String surname = "";
-        boolean geneticBackground = false;
-        while (true) {
-            System.out.print("Name: ");
-            name = sc.nextLine().trim();
-            if (!name.isEmpty()) {
-                break;
-            } else {
-                System.out.println("Invalid name. Please enter a valid name.");
-            }
-        }
-        while (true) {
-            System.out.print("Surname: ");
-            surname = sc.nextLine().trim();
-            if (!surname.isEmpty()) {
-                break;
-            } else {
-                System.out.println("Invalid surname. Please enter a valid surname.");
-            }
-        }
-        while (true) {
-            System.out.print("Genetic Background (true/false): ");
-            String input = sc.nextLine().trim().toLowerCase();
-            if (input.equals("true")) {
-                geneticBackground = true;
-                break;
-            } else if (input.equals("false")) {
-                geneticBackground = false;
-                break;
-            } else {
-                System.out.println("Invalid input. Please enter 'true' or 'false'.");
-            }
-        }
-
-        String patientData = name + "|" + surname + "|" + geneticBackground;
-        printWriter.println(patientData);
-        System.out.println("Patient data sent to the server for registration.");
-        patient = new Patient(name, surname, geneticBackground);
-    }
-
-
 
     private static void openMedicalRecord() {
         patient.openRecord();
